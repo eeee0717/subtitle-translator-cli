d5accec1a8eb787350358bba85b12755fee4a1ef
eeee0717 <chentao020717Work@outlook.com>
2024-10-21 14:02:18 +0800

    feat: refactor init

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,3 @@
-use clap::Parser;
-use subtitle_translator_cli::{process_file, Args};
-
 fn main() {
-    let args = Args::parse();
-
-    match process_file(args.path, args.source_language, args.target_language) {
-        Ok(_) => println!("Finished"),
-        Err(e) => eprintln!("Translation error: {}", e),
-    }
+    println!("Hello, world!");
 }
--- src/openai.rs
+++
@@ -1,7 +0,0 @@
-use std::error::Error;
-
-use async_openai::{
-    config::OpenAIConfig,
-    types::{ChatCompletionRequestSystemMessageArgs, CreateChatCompletionRequestArgs},
-    Client,
-};
